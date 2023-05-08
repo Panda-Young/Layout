0c4ad0a5be32492fd97f63672174e7606b80f363
Panda-Young <1641140221@qq.com>
2023-05-09 01:48:44 +0800

    leap year

--- src/lisence.c
+++ src/lisence.c
@@ -19,12 +19,7 @@
  */
 int is_leap_year(int year)
 {
-    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-        return 1;
-    }
-    else {
-        return 0;
-    }
+    return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
 }
 
 /**
